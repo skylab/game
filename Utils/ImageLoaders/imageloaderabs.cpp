e2245c77885562eaa2bbc34dc3150ceadaa346d7
unknown <yehor.shevchenko@LWO1-DHP-F46000.synapse.com>
2014-10-27 18:07:34 +0200

    Added libjpeg to work with image, need to ude it

--- Utils/ImageLoaders/imageloaderabs.cpp
+++ Utils/ImageLoaders/imageloaderabs.cpp
@@ -9,3 +9,13 @@ ImageLoaderAbs::~ImageLoaderAbs()
 {
     ;
 }
+
+bool ImageLoaderAbs::LoadImageFile(const char *filename, Texture *texture)
+{
+    // To disable warnings
+    (void)filename;
+    (void)texture;
+
+    std::cerr << "ImageLoaderAbs. Call pure virtual function" << std::endl;
+    return false;
+}
--- Utils/ImageLoaders/imageloaderabs.h
+++ Utils/ImageLoaders/imageloaderabs.h
@@ -1,11 +1,15 @@
 #ifndef IMAGELOADERABS_H
 #define IMAGELOADERABS_H
 
+#include "../../GraphicSystem/Testure/texture.h"
+
 class ImageLoaderAbs
 {
 protected:
     ImageLoaderAbs();
     virtual ~ImageLoaderAbs();
+
+    virtual bool LoadImageFile(const char *filename, Texture *texture) = 0;
 };
 
 #endif // IMAGELOADERABS_H
